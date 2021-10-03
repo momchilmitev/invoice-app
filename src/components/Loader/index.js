e88a6561db94cb08b1819c2edbd0d2a81efe29f2
momchilmitev <momchilmitev87@gmail.com>
2021-10-03 15:19:13 +0200

    adding the loader component with styles

---
+++ src/components/Loader/index.js
@@ -0,0 +1,12 @@
+import './Loader.scss';
+
+const Loader = () => {
+    return (
+        <div class="spinner">
+        <div></div>
+        <div></div>
+      </div>
+    );
+}
+
+export default Loader;
