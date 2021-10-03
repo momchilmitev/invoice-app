c03d54c0260880c7d58c3205742d9ee416c6a80c
momchilmitev <momchilmitev87@gmail.com>
2021-10-03 16:19:46 +0200

    adding all pages wtih basic markup and react ruter with lazy loading for the pages

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,11 @@
-const { resolve } = require('path');
+const { resolve } = require('path')
 
 module.exports = {
-    webpack: {
-        alias: {
-            '@components': resolve(__dirname, 'src/components/'),
-            '@assets': resolve(__dirname, 'src/assets/')
-        }
-    }
+  webpack: {
+    alias: {
+      '@components': resolve(__dirname, 'src/components/'),
+      '@assets': resolve(__dirname, 'src/assets/'),
+      '@pages': resolve(__dirname, 'src/pages/'),
+    },
+  },
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,25 @@
-import './App.scss';
+import { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.scss'
+import Loader from '@components/Loader'
+import HomePage from '@pages/Home'
+const CreatePage = lazy(() => import('@pages/Create'))
+const EditPage = lazy(() => import('@pages/Edit'))
+const ViewPage = lazy(() => import('@pages/View'))
 
 function App() {
   return (
-    <div className="app">
-      Hi Momchil
-    </div>
-  );
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/create" component={CreatePage} />
+          <Route path="/edit" component={EditPage} />
+          <Route path="/view" component={ViewPage} />
+        </Switch>
+      </Suspense>
+    </Router>
+  )
 }
 
-export default App;
+export default App
