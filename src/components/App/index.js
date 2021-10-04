021bc620becd48ae4d951330e4fa95a3b170e67f
momchilmitev <momchilmitev87@gmail.com>
2021-10-04 13:21:55 +0200

    adding the firestore and auth services to the firebase config file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,11 @@
-import { Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.scss'
-import Loader from '@components/Loader'
-import HomePage from '@pages/Home'
-const CreatePage = lazy(() => import('@pages/Create'))
-const EditPage = lazy(() => import('@pages/Edit'))
-const ViewPage = lazy(() => import('@pages/View'))
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
+import Loader from '@components/Loader';
+import HomePage from '@pages/Home';
+const CreatePage = lazy(() => import('@pages/Create'));
+const EditPage = lazy(() => import('@pages/Edit'));
+const ViewPage = lazy(() => import('@pages/View'));
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         </Switch>
       </Suspense>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,19 +1,17 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAZUqjYLxzJWfGkjlpS7BK_DwjHfCh0ewY',
-
   authDomain: 'invoice-app-214ba.firebaseapp.com',
-
   projectId: 'invoice-app-214ba',
-
   storageBucket: 'invoice-app-214ba.appspot.com',
-
   messagingSenderId: '583597045386',
-
   appId: '1:583597045386:web:26dcc00504e4e0a55d1f73',
-
   measurementId: 'G-4JHZTLSMY1',
-}
+};
 
-const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
