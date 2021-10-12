24a10c6a7aae0cd3f76f08a19c5f31c676f953b6
momchilmitev <momchilmitev87@gmail.com>
2021-10-12 14:00:49 +0200

    adding the info section with styles in the home page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <div>Home</div>
+import './Home.scss';
+import InvoiceFilter from '../components/InvoiceFilter';
+
+const Home = ({ invoices = [] }) => {
+  return <>
+      <section class="info">
+      <h2>
+        Invoices
+        <span>{ invoices.length ? invoices.length : "No" } invoices</span>
+      </h2>
+      <div class="info__actions">
+        <InvoiceFilter />
+        <button className="btn btn--create">New</button>
+      </div>
+    </section>
+  </>
 }
 
 export default Home
